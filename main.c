06442d2ae21242926524d440592b996606f54a8e
fdaryn-h <syndima43@yandex.ru>
2019-10-26 20:25:42 +0300

    added init function

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include "lem_in.h"
 
-int main()
+t_room	*init(char *name, int x, int y)
+{
+	t_room	*room;
+
+	room = ft_memalloc(sizeof(t_room));
+	room->x = x;
+	room->y = y;
+	room->name = ft_strdup(name);
+	return (room);
+}
+
+int		main()
 {
 	return 0;
 }
