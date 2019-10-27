a552d1106c39bdad0b322451a3f2b581c9a30912
Gian Brandon stark <gbrandon@student.42.fr>
2019-10-27 22:06:10 +0300

    add raw edmonds_karp

--- lem_in.h
+++ lem_in.h
@@ -3,19 +3,23 @@
 # include "libft.h"
 # include "avlt.h"
 
-struct	s_rlist
+typedef struct	s_rlist
 {
-	char			*name;
+	int				id;
+	int				flow;
+	int				cap;
 	struct	s_rlist	*first;
 	struct	s_rlist	*last;
 	struct	s_rlist	*current;
 	struct	s_rlist	*next;
-};
+}				t_rlist;
 
 typedef struct	s_room
 {
+	int				id;
 	char			*name;
-	int 			mark;
+	char 			mark;
+	int				parent;
 	int				x;
 	int				y;
 	struct	s_rlist	*lst;
@@ -24,5 +28,7 @@ typedef struct	s_room
 t_room			*get_v(t_avlt *tr, char *name);
 int				ft_bfs(t_avlt *graph, char *start, char *end);
 void			*q_input(void *it);
+int				ft_bfs_int(t_room **graph, int start, int end);
+int				edm_karp_alg(t_room **graph, int start, int end);
 
 #endif
